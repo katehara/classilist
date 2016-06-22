1b6bc1d6f5df1ae3b1152d25ce2fc01f9cc840bc
katehara <medhakat95@gmail.com>
2016-06-22 14:23:35 +0530

    Implementation Using handwriting dataset

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
 	  });
 	   
 
-	d3.csv("data/rapidminer.csv", function (error, data) {
+	d3.csv("data/prob.csv", function (error, data) {
 
 		var model = new Model(data);
 		// console.log(model.features);
